f9fce21f10518e934dca5a4ea308e41005af429c
Ben Garnett <mail@bengarnett.com>
2020-03-02 21:18:54 -0600

    make prettier

--- pages/index.js
+++ pages/index.js
@@ -1,117 +1,116 @@
 function Index() {
- return <div>
+  return (
+    <div>
+      <Game />
+      <style jsx global>
+        {`
+          body {
+            font: 14px "Century Gothic", Futura, sans-serif;
+            margin: 20px;
+          }
 
-     <Game />
-     <style jsx global>{`
-     body {
-  font: 14px "Century Gothic", Futura, sans-serif;
-  margin: 20px;
-}
+          ol,
+          ul {
+            padding-left: 30px;
+          }
 
-ol, ul {
-  padding-left: 30px;
-}
+          .board-row:after {
+            clear: both;
+            content: "";
+            display: table;
+          }
 
-.board-row:after {
-  clear: both;
-  content: "";
-  display: table;
-}
+          .status {
+            margin-bottom: 10px;
+          }
 
-.status {
-  margin-bottom: 10px;
-}
+          .square {
+            background: #fff;
+            border: 1px solid #999;
+            float: left;
+            font-size: 24px;
+            font-weight: bold;
+            line-height: 34px;
+            height: 34px;
+            margin-right: -1px;
+            margin-top: -1px;
+            padding: 0;
+            text-align: center;
+            width: 34px;
+          }
 
-.square {
-  background: #fff;
-  border: 1px solid #999;
-  float: left;
-  font-size: 24px;
-  font-weight: bold;
-  line-height: 34px;
-  height: 34px;
-  margin-right: -1px;
-  margin-top: -1px;
-  padding: 0;
-  text-align: center;
-  width: 34px;
-}
+          .square:focus {
+            outline: none;
+          }
 
-.square:focus {
-  outline: none;
-}
+          .kbd-navigation .square:focus {
+            background: #ddd;
+          }
 
-.kbd-navigation .square:focus {
-  background: #ddd;
-}
+          .game {
+            display: flex;
+            flex-direction: row;
+          }
 
-.game {
-  display: flex;
-  flex-direction: row;
-}
-
-.game-info {
-  margin-left: 20px;
-}
-
-    `} </style>
- </div>
+          .game-info {
+            margin-left: 20px;
+          }
+        `}
+      </style>
+    </div>
+  );
 }
 export default Index;
 
 class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-          {this.props.value}
-        </button>
-      );
-    }
+  render() {
+    return <button className="square">{this.props.value}</button>;
   }
-  
-  class Board extends React.Component {
-    renderSquare(i) {
-      return <Square value={i} />;
-    }
-  
-    render() {
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
+
+  render() {
+    const status = "Next player: X";
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
         </div>
-      );
-    }
-  }
-  
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
         </div>
-      );
-    }
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
   }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
